c39ae2ca7c7b8efa26da7033428e5caa7bb659a7
iEll10t <150898886+iell10t@users.noreply.github.com>
2023-12-18 14:35:37 +0400

    add multiple pages

---
+++ Project Lucy.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Project Lucy",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Project Lucy! 👋")
+
+st.markdown(
+    """
+    ## Hello Friend
+"""
+)
---
+++ README.md
@@ -0,0 +1,1 @@
+# Project Lucy (powered by Streamlit)
